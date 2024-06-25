8acd438313a7b9f6bcc5648b90930356e94121a8
luminecs <wangxuke.china@gmail.com>
2024-06-25 16:29:31 +0800

    commit

--- pkgs/dart/lib/ffi/foreign_function_interface.dart
+++ pkgs/dart/lib/ffi/foreign_function_interface.dart
@@ -1,2 +1,12 @@
 import 'dart:ffi';
-import 'package:ffi/ffi.dart';
+
+typedef CFunc = Int32 Function(Int32);
+typedef DartFunc = int Function(int);
+
+void main() {
+  final dylib = DynamicLibrary.open('path/to/library.so');
+  final DartFunc square =
+      dylib.lookup<NativeFunction<CFunc>>('square').asFunction<DartFunc>();
+
+  print(square(4)); // 16
+}
