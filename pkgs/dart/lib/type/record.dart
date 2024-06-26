7ba89d34bf9d29d952dd34a00afe8eda7e860c00
luminecs <wangxuke.china@gmail.com>
2024-06-26 11:27:39 +0800

    commit

--- pkgs/dart/lib/type/record.dart
+++ pkgs/dart/lib/type/record.dart
@@ -1,6 +1,10 @@
-var record = ('first', a: 2, b: true, 'last');
+var r = ('first', a: 2, b: true, 'last');
 
 (int, int) swap((int, int) r) {
   var (a, b) = r;
   return (b, a);
 }
+
+(String, int) record = ('', 123);
+
+({int a, bool b}) record2 = (a: 1, b: false);
