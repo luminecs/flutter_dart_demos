5375e6e4c14191e8129eca83f2342171b6b67a26
luminecs <wangxuke.china@gmail.com>
2024-06-27 09:12:39 +0800

    commit

--- pkgs/web_scss/test/web_scss_test.dart
+++ pkgs/web_scss/test/web_scss_test.dart
@@ -1,8 +1,8 @@
-import 'package:web_scss/web_scss.dart';
+// import 'package:web_scss/web_scss.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    expect(calculate(), 42);
+    // expect(calculate(), 42);
   });
 }
